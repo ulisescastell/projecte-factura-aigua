a528b0861c987ea1083b2d8bb525f577bd7064e2
ulisescastell <ulises.castell.7e7@itb.cat>
2024-01-10 19:21:35 +0100

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,14 @@
 //TIP Press <shortcut raw="SHIFT"/> twice to open the Search Everywhere dialog and type <b>show whitespaces</b>,
 // then press <shortcut raw="ENTER"/>. You can now see whitespace characters in your code.
+
+/**
+ * @author ulises_castell
+ */
+
 fun main() {
     showMenu()
     println(socialBonus())
     println(calculateConsumptionWater(200.0))
-
 
     val provaUser = readInt("Introdueix la el teu consum de aigua al mes:", "Format incorrecte")
     val consumption =
