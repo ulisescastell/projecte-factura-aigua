6f120e963058a2eb66389fc8999fec85bd751888
ulises.castell.7e7 <ulisescastell6@gmail.com>
2024-01-10 10:32:30 +0100

    add new file applicationsMsg.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-package org.example
 //TIP Press <shortcut raw="SHIFT"/> twice to open the Search Everywhere dialog and type <b>show whitespaces</b>,
 // then press <shortcut raw="ENTER"/>. You can now see whitespace characters in your code.
 fun main() {
-    println("Bon día anem a començar;")
-    println()
+    showMenu()
+    println(calculateConsumptionWater(200.0))
 }
--- src/main/kotlin/mainFunctions.kt
+++ src/main/kotlin/mainFunctions.kt
@@ -13,3 +13,15 @@ fun calculateConsumptionWater (consumption: Double): Double {
     }
     return consumption
 }
+
+fun areYouLargeFamily () {
+    val familyOption = readInt("Ets familia nombrosa(1), monomarental(2)? No perteneixc a cap(3)", pMessageErrorDT = "Input incorrecte")
+    if (familyOption == 1 || familyOption == 2) {
+        val numOfFamilyMembers = readInt("Perfecte! Tens descompte. De quants de membres estem parlant?", pMessageErrorDT = "Input incorrecte")
+        when (numOfFamilyMembers) {
+            1 -> calculateConsumptionWater(consumption = )
+        }
+}
+
+
+}
