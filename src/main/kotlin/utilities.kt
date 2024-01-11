b67ce2436f12e01d5839007d586aac4a9441144b
ulisescastell <ulises.castell.7e7@itb.cat>
2024-01-11 17:38:53 +0100

    clonate function readFloat and delete pMax parameter

--- src/main/kotlin/utilities.kt
+++ src/main/kotlin/utilities.kt
@@ -296,3 +296,43 @@ fun readFloat(pMessageIn: String
     return outputValue
 }
 
+/**
+ * This method can be used to read a Float value from the user through keyboard using java.util.Scanner with a minValue
+ * @author raimon.izard
+ * @author ulisescastell
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @param pMessageErrorDV Data value error message to be shown to the user
+ * @param pMin Min accepted value
+ * @return outputValue Output value
+ */
+fun readFloat(pMessageIn: String
+              , pMessageErrorDT: String
+              , pMessageErrorDV: String
+              , pMin: Float
+): Float{
+
+    var outputValue: Float = 0.0f
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextFloat()
+
+        if (!correctDataType){
+            println(RED_BACKGROUND_BRIGHT + "ERROR: " + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextFloat()
+
+            if (outputValue < pMin){
+                println(YELLOW_BOLD_BRIGHT + "WARNING: " + pMessageErrorDV + RESET)
+                correctDataType = false
+            }
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
