0a45bc0e5dfe4413c01877ab9452af30a501adc1
ulisescastell <ulises.castell.7e7@itb.cat>
2024-01-11 20:43:44 +0100

    edit fun showFinalInvoice and end basic structure of project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,26 @@
 import kotlin.math.roundToInt
 
-/**
- * @author ulises_castell
- */
-
 fun main() {
     showMenu()
-    val consumption = readFloat("Introdueix el consum d'aigua mensual", "resposta incorrecte", "Valor massa petit. Has d'introduïr un nombre no inferior a 0.", 0.0f)
-    calculateConsumptionWater(consumption)
-    val hasSocialBonus = readBoolean("Tens algun bonus social? Respon true en cas afirmatiu i false en cas negatiu", "resposta incorrecte")
-    socialBonus(consumption, hasSocialBonus)
-    if (!hasSocialBonus) {
-        calculateLargeFamily()
+    val consumption = readFloat("Introdueix el consum d'aigua mensual: ", "Resposta incorrecte", "Valor massa petit. Has d'introduïr un nombre no inferior a 0.", 0.0f)
+    var totalConsumption:Float = 0.0f
+    var waterConsumption:Float = 0.0f
+    var hasSocialBonus:Boolean = false
+    var largeFamilyDiscount:Float = 3.0f
+    if (consumption >= 50) {
+        waterConsumption = calculateConsumptionWater(consumption)
+        hasSocialBonus = readBoolean("Tens algun bonus social? Respon true en cas afirmatiu i false en cas negatiu.", "Resposta incorrecte")
+        val socialBonusDiscount = socialBonus(consumption, hasSocialBonus)
+        if (!hasSocialBonus) {
+            largeFamilyDiscount = calculateLargeFamily()
+            totalConsumption += waterConsumption * largeFamilyDiscount
+        }
+        else {
+            totalConsumption += waterConsumption * socialBonusDiscount
+        }
     }
-    println("Has de pagar $fixedFee EUR de quota fixa.")
+    totalConsumption += fixedFee
+    showFinalInvoice(finalPrice = totalConsumption, waterPrice = waterConsumption, consumedLiters = consumption, haveUSocialBonus = hasSocialBonus, areULongFamily = largeFamilyDiscount )
 }
 
 
--- src/main/kotlin/applicationsMsg.kt
+++ src/main/kotlin/applicationsMsg.kt
@@ -1,12 +1,35 @@
 fun showMenu() {
-    val str = CYAN_BOLD_BRIGHT + """
+    val str = PURPLE_BOLD + """
     |##################################################
     |#                                                #
-    |#   Benvingut a la factura de la llum!           #
+    |#   Benvingut a la factura de l'aigua!           #
     |#                                                #
     |##################################################
     """.trimMargin() + RESET
     println(str)
 }
+
+fun showFinalInvoice(finalPrice: Float, waterPrice: Float, consumedLiters: Float, haveUSocialBonus: Boolean, areULongFamily: Float) {
+
+    println("$PURPLE_BOLD########################################")
+    println("#        AQUÍ ESTÁ LA TEVA FACTURA     #")
+    println("########################################")
+    println("# Cost total: ${roundFloat(finalPrice)} EUR          ")
+    println("# Quota fixa: $fixedFee EUR             ")
+    println("# Consum: $consumedLiters litros        ")
+    println("# Cost aigua: ${roundFloat(waterPrice)} EUR        ")
+    if (haveUSocialBonus) {
+        println("# BONUS SOCIAL APLICAT: -80%           ")
+    } else {
+        println("# Sense descompte per bonus social                 ")
+        if (areULongFamily != 3.0f) {
+            println("# Descompte familiar: ${100 - (areULongFamily * 100)}% ")
+        } else {
+            println("# Sense descompte familiar             ")
+        }
+    }
+    println("########################################$RESET")
+}
+
 
 
