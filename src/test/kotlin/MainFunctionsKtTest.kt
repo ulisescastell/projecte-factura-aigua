5bebb69a099bc995273734155a351108ca676797
ulisescastell <ulises.castell.7e7@itb.cat>
2024-01-11 23:23:19 +0100

    test of calculateConsumptionWater function

--- src/test/kotlin/MainFunctionsKtTest.kt
+++ src/test/kotlin/MainFunctionsKtTest.kt
@@ -1,3 +1,18 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class MainFunctionsKtTest
+class MainFunctionsKtTest {
+
+    //per a funció calculateConsumptionWater
+    @Test
+    fun checkMediumConsumption() {
+        val expected = 100f * 0.15f
+        assertEquals(expected, calculateConsumptionWater(100f), 0.0f)
+    }
+    @Test
+    fun checkHighConsumption() {
+        assertTrue(calculateConsumptionWater(250f) == 250f * 0.3f)
+    }
+
+
+}
